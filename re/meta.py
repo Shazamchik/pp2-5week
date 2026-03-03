9b92e38745eedf170c514f98a04f67c8f6876a0b
BeibarysShayakhmetov <shazam.chik88@mail.ru>
2026-03-03 19:46:40 +0500

    changed something and add extra examples

--- re/meta.py
+++ re/meta.py
@@ -5,3 +5,4 @@ print(re.findall(r"^Hello", "Hello World"))  # ^ (start)
 print(re.findall(r"World$", "Hello World"))  # $ (end)
 print(re.findall(r"[ae]", "apple"))  # [] 
 print(re.findall(r"cat|dog", "cat dog"))  # | (OR)
+print(re.findall(r"He.{2}o", "Hello World")) # Exactly the specified number of occurrences
